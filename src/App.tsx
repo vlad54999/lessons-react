0b9dfd4461356239f8a9735bdbb09898da16d318
vlad54999 <vlad54999@gmail.com>
2026-08-12 12:42:16 +0200

    Add First Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import FirstComponent from "./components/MyFirstComponents.tsx";
 
 function App() {
 
   return (
-    <></>
+    <>
+      Hello Vlad!!!!
+      <FirstComponent text={'Hello Vlad'}/>
+      {FirstComponent({text: 'Hello World!!!!'})}
+    </>
   )
 }
 
---
+++ src/components/MyFirstComponents.tsx
@@ -0,0 +1,10 @@
+import '../App.css';
+
+type MyComponentTestType = { text: string };
+
+function FirstComponent({text}: MyComponentTestType){
+    return <div>{text}</div>
+
+}
+
+export default FirstComponent;
