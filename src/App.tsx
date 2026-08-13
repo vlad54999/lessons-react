15cfbb47ece72bcd6138fd0ae51e48ef33c51641
vlad54999 <vlad54999@gmail.com>
2026-08-13 09:26:58 +0200

    HW_1 Task_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+// #gi01MZ6v
+//
+// з arrays.js взяти масив coursesTitleArray. Вивести кожен його елемент, створивши необхідні моделі та компоненти
+//
+// Стилізація довільна, але обов’язково підключена і мінімально зроблена (хоча б один стиль, який працює).
+
 import './App.css'
-import FirstComponent from "./components/MyFirstComponents.tsx";
 
-function App() {
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
-  return (
+const App = () => {
+    return (
     <>
-      Hello Vlad!!!!
-      <FirstComponent text={'Hello Vlad'}>Lorem ipsum dolor.</FirstComponent>
-      {FirstComponent({text: 'Hello World!!!!'})}
+        <ul>
+            {
+                coursesTitleArray.map((course, index) => <li key={index}>{course}</li>)
+            }
+        </ul>
     </>
-  )
+  );
+
 }
+
 
 export default App
