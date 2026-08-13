1709a7dd5a39ccfe709375d18d1191bcd5fce342
vlad54999 <vlad54999@gmail.com>
2026-08-13 14:27:52 +0200

    HW_1 Task_4

--- src/components/courses-component/CoursesComponent.tsx
+++ src/components/courses-component/CoursesComponent.tsx
@@ -1,6 +1,13 @@
 import {coursesArray} from "../../data/data.ts";
 import {CourseComponent} from "../course-component/CourseComponent.tsx";
 import './CoursesComponent.css'
+
+// #yiugR7&TF
+//
+// з arrays.js взяти масиви coursesArray. Створити необхідні моделі та компоненти для того, щоб повноцінно відобразити його
+//
+// Стилізація довільна, але обов'язково підключена і мінімально зроблена (хоча б один стиль, який працює).
+//
 export const CoursesComponent = () => {
     return (
         <div>
